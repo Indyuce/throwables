db686672f7b57ef2826f38153cfd22428336324b
Indyuce <cym.peyrat@yahoo.fr>
2021-08-21 23:35:08 +0200

    removed version dependant code

--- src/main/java/fr/Indyuce/throwables/Throwables.java
+++ src/main/java/fr/Indyuce/throwables/Throwables.java
@@ -1,15 +1,18 @@
 package fr.Indyuce.throwables;
 
 import fr.Indyuce.throwables.api.ThrowablesAPI;
-import fr.Indyuce.throwables.command.ThrowablesCommandExecutor;
+import fr.Indyuce.throwables.command.ThrowablesExecutor;
+import fr.Indyuce.throwables.command.ThrowablesCompleter;
+import fr.Indyuce.throwables.compat.Metrics;
+import fr.Indyuce.throwables.compat.color.ColorParser;
+import fr.Indyuce.throwables.compat.color.HexColorParser;
+import fr.Indyuce.throwables.compat.color.SimpleColorParser;
 import fr.Indyuce.throwables.listener.PlayerListener;
 import fr.Indyuce.throwables.listener.ThrowablesListener;
 import fr.Indyuce.throwables.manager.ThrowableManager;
 import fr.Indyuce.throwables.manager.ThrownItemManager;
 import fr.Indyuce.throwables.player.PlayerData;
 import fr.Indyuce.throwables.version.ServerVersion;
-import fr.Indyuce.throwables.version.wrapper.VersionWrapper;
-import fr.Indyuce.throwables.version.wrapper.VersionWrapper_1_17_R1;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -22,13 +25,25 @@ public class Throwables extends JavaPlugin {
     public final ThrownItemManager thrownItemManager = new ThrownItemManager();
     public final ThrowablesAPI api = new ThrowablesAPI(this);
 
-    public VersionWrapper versionWrapper = new VersionWrapper_1_17_R1();
+    public ColorParser colorParser;
     public ServerVersion version;
 
     @Override
     public void onEnable() {
 
         plugin = this;
+
+        // Update checker
+        /* new SpigotPlugin(1111, this);*/
+
+        // Metrics
+        new Metrics(this);
+
+        // Find plugin version
+        version = new ServerVersion(Bukkit.getServer().getClass());
+
+        // Hex color support
+        colorParser = version.isStrictlyHigher(1, 15) ? new HexColorParser() : new SimpleColorParser();
 
         // Register events
         Bukkit.getPluginManager().registerEvents(new ThrowablesListener(), this);
@@ -38,7 +53,8 @@ public class Throwables extends JavaPlugin {
         Bukkit.getOnlinePlayers().forEach(online -> PlayerData.setup(online));
 
         // Plugin commands
-        getCommand("throwables").setExecutor(new ThrowablesCommandExecutor());
+        getCommand("throwables").setExecutor(new ThrowablesExecutor());
+        getCommand("throwables").setTabCompleter(new ThrowablesCompleter());
 
         // Default config
         saveDefaultConfig();
